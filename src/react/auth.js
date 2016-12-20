25fb925902bc6980ca379567baa024d41256a19f
Marshall Thompson <marshall@creativeideal.net>
2016-12-20 00:13:13 -0700

    Remove react version.

--- src/react/auth.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-view-models';
-import DefineMap from 'can-define/map/';
-import Template from './auth.jsx';
-import './auth.less';
-
-export const ViewModel = DefineMap.extend({
-});
-
-export default connect(ViewModel, Template);
