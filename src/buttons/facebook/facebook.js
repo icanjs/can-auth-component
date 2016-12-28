35fca589a6cd78074814fe892416cf7e4aa1feda
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:01:23 -0700

    Facebook button

--- src/buttons/facebook/facebook.js
+++ src/buttons/facebook/facebook.js
@@ -1,4 +1,27 @@
-import FacebookButton from './facebook.jsx';
-import './facebook.less';
+import Component from 'can-component';
+import view from './facebook.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './facebook.svg';
 
-export default FacebookButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Facebook'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/facebook'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'facebook-auth-button',
+  ViewModel,
+  view
+});
+
