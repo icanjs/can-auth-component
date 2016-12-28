5f123f56b675f058756c78f301f38cb72b1449c1
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:03:40 -0700

    LinkedIn button

--- src/buttons/linkedin/linkedin.js
+++ src/buttons/linkedin/linkedin.js
@@ -1,4 +1,27 @@
-import LinkedInButton from './linkedin.jsx';
-import './linkedin.less';
+import Component from 'can-component';
+import view from './linkedin.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './linkedin.svg';
 
-export default LinkedInButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'LinkedIn'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/linkedin'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'linkedin-auth-button',
+  ViewModel,
+  view
+});
+
