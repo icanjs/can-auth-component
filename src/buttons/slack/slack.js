c4933300b82b83b3256a2b552dee33f402eb2ab8
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:08:26 -0700

    Slack button

--- src/buttons/slack/slack.js
+++ src/buttons/slack/slack.js
@@ -1,4 +1,27 @@
-import SlackButton from './slack.jsx';
-import './slack.less';
+import Component from 'can-component';
+import view from './slack.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './slack.svg';
 
-export default SlackButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Slack'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/slack'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'slack-auth-button',
+  ViewModel,
+  view
+});
+
