cbccb0c29a153629b4842ab5238f172521a24935
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 22:58:24 -0700

    Update evernote button

--- src/buttons/evernote/evernote.js
+++ src/buttons/evernote/evernote.js
@@ -1,4 +1,27 @@
-import EvernoteButton from './evernote.jsx';
-import './evernote.less';
+import Component from 'can-component';
+import view from './evernote.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './evernote.svg';
 
-export default EvernoteButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Evernote'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/evernote'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'evernote-auth-button',
+  ViewModel,
+  view
+});
+
