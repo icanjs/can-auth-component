03fecede46cf45f32253a02e7519f8505a304e23
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:07:00 -0700

    Openid button

--- src/buttons/openid/openid.js
+++ src/buttons/openid/openid.js
@@ -1,4 +1,27 @@
-import OpenIDButton from './openid.jsx';
-import './openid.less';
+import Component from 'can-component';
+import view from './openid.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './openid.svg';
 
-export default OpenIDButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'OpenID'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/openid'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'openid-auth-button',
+  ViewModel,
+  view
+});
+
