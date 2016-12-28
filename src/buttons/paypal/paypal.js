b3ae40fa6b3f08ae49111f09d353c37220d15256
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:07:08 -0700

    paypal button

--- src/buttons/paypal/paypal.js
+++ src/buttons/paypal/paypal.js
@@ -1,4 +1,27 @@
-import PaypalButton from './paypal.jsx';
-import './paypal.less';
+import Component from 'can-component';
+import view from './paypal.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './paypal.svg';
 
-export default PaypalButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'PayPal'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/paypal'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'paypal-auth-button',
+  ViewModel,
+  view
+});
+
