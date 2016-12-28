2970afd3cb7ac7db5100e91176d59c10b1afc3f5
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:07:33 -0700

    Skype button

--- src/buttons/skype/skype.js
+++ src/buttons/skype/skype.js
@@ -1,4 +1,27 @@
-import SkypeButton from './skype.jsx';
-import './skype.less';
+import Component from 'can-component';
+import view from './skype.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './skype.svg';
 
-export default SkypeButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Skype'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/skype'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'skype-auth-button',
+  ViewModel,
+  view
+});
+
