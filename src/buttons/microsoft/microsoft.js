f05ed92a1e739d6e2e4a1ddf5ca098c817b72e5e
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:04:36 -0700

    Microsoft Button

--- src/buttons/microsoft/microsoft.js
+++ src/buttons/microsoft/microsoft.js
@@ -1,4 +1,27 @@
-import MicrosoftButton from './microsoft.jsx';
-import './microsoft.less';
+import Component from 'can-component';
+import view from './microsoft.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './microsoft.svg';
 
-export default MicrosoftButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Microsoft'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/microsoft'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'microsoft-auth-button',
+  ViewModel,
+  view
+});
+
