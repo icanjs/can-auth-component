6813c23562c7555477ebdb31702ef8578f1f7fda
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:09:30 -0700

    Stackoverflow button

--- src/buttons/stackoverflow/stackoverflow.js
+++ src/buttons/stackoverflow/stackoverflow.js
@@ -1,4 +1,27 @@
-import StackOverflowButton from './stackoverflow.jsx';
-import './stackoverflow.less';
+import Component from 'can-component';
+import view from './stackoverflow.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './stackoverflow.svg';
 
-export default StackOverflowButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Stack Overflow'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/stackoverflow'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'stackoverflow-auth-button',
+  ViewModel,
+  view
+});
+
