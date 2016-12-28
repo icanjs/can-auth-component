de3c720130952631a18201a695b0fecefc10df86
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 22:57:55 -0700

    Dropbox button

--- src/buttons/dropbox/dropbox.js
+++ src/buttons/dropbox/dropbox.js
@@ -1,4 +1,27 @@
-import DropboxButton from './dropbox.jsx';
-import './dropbox.less';
+import Component from 'can-component';
+import view from './dropbox.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './dropbox.svg';
 
-export default DropboxButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Dropbox'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/dropbox'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'dropbox-auth-button',
+  ViewModel,
+  view
+});
+
