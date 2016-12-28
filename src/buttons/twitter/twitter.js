d2a6c90044d3cc535707e437616f7ea6bdc02a8f
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:10:27 -0700

    Twitter button

--- src/buttons/twitter/twitter.js
+++ src/buttons/twitter/twitter.js
@@ -1,4 +1,27 @@
-import TwitterButton from './twitter.jsx';
-import './twitter.less';
+import Component from 'can-component';
+import view from './twitter.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './twitter.svg';
 
-export default TwitterButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Twitter'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/twitter'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'twitter-auth-button',
+  ViewModel,
+  view
+});
+
