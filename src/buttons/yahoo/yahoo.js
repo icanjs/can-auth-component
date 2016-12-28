f5c3eda1077ad81337990b0cc1f3ae38a377a67a
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:14:25 -0700

    Yahoo button

--- src/buttons/yahoo/yahoo.js
+++ src/buttons/yahoo/yahoo.js
@@ -1,4 +1,27 @@
-import YahooButton from './yahoo.jsx';
-import './yahoo.less';
+import Component from 'can-component';
+import view from './yahoo.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './yahoo.svg';
 
-export default YahooButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Twitter'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/yahoo'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'yahoo-auth-button',
+  ViewModel,
+  view
+});
+
