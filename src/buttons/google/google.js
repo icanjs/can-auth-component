6334fc36aff409b7f4ccfe7730a3594d8a49efc8
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 23:02:31 -0700

    Google button

--- src/buttons/google/google.js
+++ src/buttons/google/google.js
@@ -1,4 +1,27 @@
-import GoogleButton from './google.jsx';
-import './google.less';
+import Component from 'can-component';
+import view from './google.stache';
+import {ViewModel as BaseViewModel} from '../button';
+import svg from './google.svg';
 
-export default GoogleButton;
+export const ViewModel = BaseViewModel.extend({
+  name: {
+    type: 'string',
+    value: 'Google'
+  },
+  url: {
+    type: 'string',
+    value: '/auth/google'
+  },
+  svg: {
+    value: svg
+  },
+  text: 'string',
+  popup: 'boolean'
+});
+
+export default Component.extend({
+  tag: 'google-auth-button',
+  ViewModel,
+  view
+});
+
