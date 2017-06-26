440f85dfe240cd063563280d42a8fd956b98f066
Marshall Thompson <marshall@creativeideal.net>
2017-06-26 08:19:37 -0600

    Use standard for linting.

--- src/can-auth-component.js
+++ src/can-auth-component.js
@@ -3,8 +3,8 @@ import './auth-container/auth-container'
 // import './forms/form/form';
 // import './forms/form-error/form-error';
 // import AsyncValidator from './forms/async-validator/async-validator';
-import './forms/login/login';
-import './forms/signup/signup';
+import './forms/login/login'
+import './forms/signup/signup'
 
 import './buttons/button'
 import './buttons/amazon/amazon'
@@ -23,4 +23,4 @@ import './buttons/stackoverflow/stackoverflow'
 import './buttons/twitter/twitter'
 import './buttons/yahoo/yahoo'
 
-import './tabs/tabs';
+import './tabs/tabs'
